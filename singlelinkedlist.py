9b08e454f4663d975379ae67883a66c021a6026c
gnehzkcaj Zheng <jiezheng@live.com.au>
2023-11-07 01:18:23 +0000

    230

---
+++ singlelinkedlist.py
@@ -0,0 +1,91 @@
+class Node:
+    """
+    An object for storing a single node in a linked list
+
+    Attributes:
+        data: Data stored in node
+        next_node: Reference to next node in linked list
+    """
+
+    def __init__(self, data, next_node = None):
+        self.data = data
+        self.next_node = next_node
+
+    def __repr__(self):
+        return "<Node data: %s>" % self.data
+
+class SinglyLinkedList:
+    """
+    Linear data structure that stores values in nodes. The list maintains a reference to the first node, also called head. Each node points to the next node in the list
+
+    Attributes:
+        head: The head node of the list
+    """
+
+    def __init__(self):
+        self.head = None
+        # Maintaining a count attribute allows for len() to be implemented in
+        # constant time
+        self.__count = 0
+
+    def is_empty(self):
+        """
+        Determines if the linked list is empty
+        Takes O(1) time
+        """
+
+        return self.head is None
+
+    def __len__(self):
+        """
+        Returns the length of the linked list
+        Takesn O(1) time
+        """
+
+        return self.__count
+
+    def add(self, data):
+        # Adds new Node
+        new_node = Node(data)
+        new_node.next_node = self.head
+        self.head = new_node
+
+    def print_list(self):
+        """
+        Prints the entire linked list.
+        Takes O(n) time, where n is the number of nodes in the list.
+        """
+        current_node = self.head
+        while current_node:
+            print(current_node.data, end=" -> ")
+            current_node = current_node.next_node
+        print("tail")
+
+    def search(self, key):
+        current = self.head
+        while current:
+            if current.data == key:
+                return current
+            else:
+                current = current.next_node
+        return None
+
+    def insert(self, data, index):
+        if index == 0:
+            self.add(data)
+
+        if index > 0:
+            new = Node(data)
+
+            position = index
+            current = self.head
+
+            while postion > 1:
+                current = node.next_node
+                position -= 1
+
+            prev_node = current
+            next_node = current.next_node
+
+            prev_node.next_node = new
+            new.next_node = next_node
