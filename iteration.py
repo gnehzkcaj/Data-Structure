b837f28298e5ba8af6f3efb329a33f044c757d2d
gnehzkcaj Zheng <jiezheng@live.com.au>
2023-11-08 01:16:04 +0000

    iteration

---
+++ iteration.py
@@ -0,0 +1,80 @@
+def for_loop(n: int) -> int:
+    """for 循环"""
+    res = 0
+    # 循环求和 1, 2, ..., n-1, n
+    for i in range(1, n + 1):
+        res += i
+    return res
+
+def while_loop(n: int) -> int:
+    """while 循环"""
+    res = 0
+    i = 1  # 初始化条件变量
+    # 循环求和 1, 2, ..., n-1, n
+    while i <= n:
+        res += i
+        i += 1  # 更新条件变量
+    return res
+
+def while_loop_ii(n: int) -> int:
+    """while 循环（两次更新）"""
+    res = 0
+    i = 1  # 初始化条件变量
+    1,1,2,4.
+    # 循环求和 1, 4, ...
+    while i <= n:
+        res += i
+        # 更新条件变量
+        i += 1
+        i *= 2
+    return res
+
+def nested_for_loop(n: int) -> str:
+    """双层 for 循环"""
+    res = ""
+    # 循环 i = 1, 2, ..., n-1, n
+    for i in range(1, n + 1):
+        # 循环 j = 1, 2, ..., n-1, n
+        for j in range(1, n + 1):
+            res += f"({i}, {j}), "
+    return res
+
+def recur(n: int) -> int:
+    """递归"""
+    # 终止条件
+    if n == 1:
+        return 1
+    # 递：递归调用
+    res = recur(n - 1)
+    # 归：返回结果
+    return n + res
+
+def tail_recur(n, res):
+    """尾递归"""
+    # 终止条件
+    if n == 0:
+        return res
+    # 尾递归调用
+    return tail_recur(n - 1, res + n)
+
+def fib(n: int) -> int:
+    """斐波那契数列：递归"""
+    # 终止条件 f(1) = 0, f(2) = 1
+    if n == 1 or n == 2:
+        return n - 1
+    # 递归调用 f(n) = f(n-1) + f(n-2)
+    res = fib(n - 1) + fib(n - 2)
+    # 返回结果 f(n)
+    return res
+
+print(fib(5))
+
+print(tail_recur(5,0))
+print('*'*8)   
+
+print(recur(5))
+print('*'*8)
+print(for_loop(5))
+print(while_loop(5))
+print(while_loop_ii(5))
+print(nested_for_loop(2))
