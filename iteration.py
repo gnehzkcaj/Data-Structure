91298fb69968078fd9268d4951ae1716e65be491
jackzheng8875 <jackzheng8875@gmail.com>
2023-11-08 23:06:35 +0000

    testidx

--- iteration.py
+++ iteration.py
@@ -67,6 +67,24 @@ def fib(n: int) -> int:
     # 返回结果 f(n)
     return res
 
+def for_loop_recur(n: int) -> int:
+    """使用迭代模拟递归"""
+    # 使用一个显式的栈来模拟系统调用栈
+    stack = []
+    res = 0
+    # 递：递归调用
+    for i in range(n, 0, -1):
+        # 通过“入栈操作”模拟“递”
+        stack.append(i)
+    # 归：返回结果
+    while stack:
+        # 通过“出栈操作”模拟“归”
+        res += stack.pop()
+    # res = 1+2+3+...+n
+    return res
+
+print(for_loop_recur(5))
+print('*'*8)  
 print(fib(5))
 
 print(tail_recur(5,0))
