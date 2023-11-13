6aa706872809c06409d84a172add3d6aefe5a0c5
gnehzkcaj Zheng <jiezheng@live.com.au>
2023-11-13 00:05:02 +0000

    loop

--- loop.py
+++ loop.py
@@ -2,14 +2,18 @@
 
 # Number to guess: How many passwords
 # can we bcrypt in a second?
-
+### To run, in terminal python loop.py 122
 import bcrypt
+import sys
+
+if len(sys.argv) < 2:
+    print("Usage: python script.py <NUMBER>")
+    sys.exit(1)
 
 password = 'a' * 100
 
 def f(NUMBER):
     for _ in range(NUMBER):
-        bcrypt.hashpw(password, bcrypt.gensalt())
-        
-import sys
+        bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
 f(int(sys.argv[1]))
